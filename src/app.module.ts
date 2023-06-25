71d710a5150417159b5d119d2a5beab3e8e1e983
Krystian Domzalowicz <krystian.domza@gmail.com>
2023-06-25 19:32:44 +0200

    feat: enable CORS & add typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { envFilePath, envConfig } from './config/env/env.config';
 import { envValidation } from './config/env/env.validation';
+import { typeOrmModuleConfig } from './config/typeOrmModule.config';
 
 @Module({
   imports: [
@@ -11,6 +13,13 @@ import { envValidation } from './config/env/env.validation';
       isGlobal: true,
       load: [envConfig],
       validationSchema: envValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return await typeOrmModuleConfig(configService);
+      },
+      inject: [ConfigService],
     }),
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { Environment } from './config/env/env.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const configService = app.get(ConfigService);
   const port = configService.get(Environment.PORT);
 
