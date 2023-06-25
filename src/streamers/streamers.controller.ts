118302a0a2fb6c59a38f279233448960d346c0df
Krystian Domzalowicz <krystian.domza@gmail.com>
2023-06-25 19:59:17 +0200

    feat:add streamers controllers

--- src/streamers/streamers.controller.ts
+++ src/streamers/streamers.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get, Post, Body } from '@nestjs/common';
+import { StreamersService } from './streamers.service';
+import { CreateStreamerDto } from './dto/CreateStreamerDto';
 
 @Controller('streamers')
-export class StreamersController {}
+export class StreamersController {
+  constructor(private readonly streamersService: StreamersService) {}
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.streamersService.findOne(id);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.streamersService.findAll();
+  }
+
+  @Post()
+  async create(@Body() createStreamerDto: CreateStreamerDto) {
+    return await this.streamersService.create(createStreamerDto);
+  }
+}
